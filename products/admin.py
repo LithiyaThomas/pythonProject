aea40ad93bef58ffd48d7d74f8c2d1b5bad89ecc
LithiyaThomas <lithiyathomas12@gmail.com>
2020-11-25 15:24:38 +0530

    Initial commit

---
+++ products/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import product
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name",'price')
+    #admin.site.register(product,ProductAdmin)
